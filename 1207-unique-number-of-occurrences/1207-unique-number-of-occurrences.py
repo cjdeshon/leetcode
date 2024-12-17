348cfaab0d613a177164bf1e5406584dc02decb0
cjdeshon <152043933+cjdeshon@users.noreply.github.com>
2024-12-17 17:50:15 -0600

    Time: 0 ms (100.00%), Space: 17.8 MB (8.12%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,11 +1,16 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        unordered_map = {}
-        for num in arr:
-            if num in unordered_map:
-                unordered_map[num] = unordered_map[num] + 1
-            else:
-                unordered_map[num] = 1
+        return len(set(Counter(arr).values())) == len(set(arr))
+        
+        
+        
+        
+#         unordered_map = {}
+#         for num in arr:
+#             if num in unordered_map:
+#                 unordered_map[num] = unordered_map[num] + 1
+#             else:
+#                 unordered_map[num] = 1
                 
-        return len(unordered_map.keys()) == len(set(unordered_map.values()))
+#         return len(unordered_map.keys()) == len(set(unordered_map.values()))
         
