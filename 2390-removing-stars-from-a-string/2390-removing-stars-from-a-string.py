6f6ee13f51578630d189867b03e6eca4a18ed75c
cjdeshon <152043933+cjdeshon@users.noreply.github.com>
2024-12-17 19:28:29 -0600

    Time: 147 ms (23.93%), Space: 18.7 MB (6.36%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        st = []
+        for i in range(len(s)):
+            if s[i] == '*':
+                st.pop()
+            else:
+                st.append(s[i])
+            
+        return ''.join(st)
