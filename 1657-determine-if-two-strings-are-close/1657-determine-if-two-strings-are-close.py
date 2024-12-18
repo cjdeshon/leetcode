430679aeeae9c44fb572cc49020ae8309880bed7
cjdeshon <152043933+cjdeshon@users.noreply.github.com>
2024-12-17 18:19:22 -0600

    Time: 85 ms (70.73%), Space: 18.1 MB (6.01%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -1,18 +1,23 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        c1,c2 = Counter(word1), Counter(word2)
         
-        word1Dict = {}
-        for char in word1:
-            if char not in word1Dict:
-                word1Dict[char] = 1
-            else:
-                word1Dict[char] += 1
+        return sorted(c1.values()) == sorted(c2.values()) and sorted(c1.keys()) == sorted(c2.keys())
+        
+        
+        
+#         word1Dict = {}
+#         for char in word1:
+#             if char not in word1Dict:
+#                 word1Dict[char] = 1
+#             else:
+#                 word1Dict[char] += 1
                 
-        word2Dict = {}
-        for char in word2:
-            if char not in word2Dict:
-                word2Dict[char] = 1
-            else:
-                word2Dict[char] += 1
+#         word2Dict = {}
+#         for char in word2:
+#             if char not in word2Dict:
+#                 word2Dict[char] = 1
+#             else:
+#                 word2Dict[char] += 1
                 
-        return sorted(word1Dict.values()) == sorted(word2Dict.values()) and (sorted(word1Dict.keys()) == sorted(word2Dict.keys()))
+#         return sorted(word1Dict.values()) == sorted(word2Dict.values()) and (sorted(word1Dict.keys()) == sorted(word2Dict.keys()))
