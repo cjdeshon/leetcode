fa4167e82eb20d79a299dfcfb97487e7fe2483c3
cjdeshon <152043933+cjdeshon@users.noreply.github.com>
2024-12-19 14:54:43 -0600

    Time: 146 ms (23.95%), Space: 32.8 MB (5.49%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,24 +6,57 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node, prevMax):
-            nonlocal numGood
-            if node:
-                if node.left:
-                    if node.val <= node.left.val and node.left.val >= prevMax:
-                        print("numG updated at node val ", node.val)
-                        numGood += 1
-                        dfs(node.left, node.left.val)
-                    else:
-                        dfs(node.left, prevMax)
-                if node.right:
-                    if node.val <= node.right.val and node.right.val >= prevMax:
-                        print("numG updated at node val ", node.val)
-                        numGood += 1
-                        dfs(node.right, node.right.val)
-                    else:
-                        dfs(node.right, prevMax)
-        numGood = 1
-        dfs(root, root.val)
+        #iterative approach
+        st = [(root, float(-inf))]
+        numGood = 0
+        
+        while st:
+            node, prevMax = st.pop()
             
+            if prevMax <= node.val:
+                numGood += 1
+            if node.left:
+                st.append((node.left, max(node.val, prevMax)))
+            if node.right:
+                          st.append((node.right, max(node.val, prevMax)))
+                          
         return numGood
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #recursive approach
+#         def dfs(node, prevMax):
+#             nonlocal numGood
+#             if node:
+#                 if node.left:
+#                     if node.val <= node.left.val and node.left.val >= prevMax:
+#                         print("numG updated at node val ", node.val)
+#                         numGood += 1
+#                         dfs(node.left, node.left.val)
+#                     else:
+#                         dfs(node.left, prevMax)
+#                 if node.right:
+#                     if node.val <= node.right.val and node.right.val >= prevMax:
+#                         print("numG updated at node val ", node.val)
+#                         numGood += 1
+#                         dfs(node.right, node.right.val)
+#                     else:
+#                         dfs(node.right, prevMax)
+#         numGood = 1
+#         dfs(root, root.val)
+            
+#         return numGood
