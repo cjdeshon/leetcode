657b329650c1a99b0e29112ee10378a2e42295e3
cjdeshon <152043933+cjdeshon@users.noreply.github.com>
2024-12-23 16:19:43 -0600

    Time: 0 ms (100.00%), Space: 17.8 MB (5.68%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -9,24 +9,49 @@ class Solution:
         if root is None:
             return []
         
-        next_level = deque([root,])
+        queue = deque( [root,])
         rightside = []
         
-        while next_level:
-            #prep for next level
-            curr_level = next_level
-            next_level = deque()
+        while queue:
+            level_length = len(queue)
             
-            while curr_level:
-                node = curr_level.popleft()
-                
-                #add children
+            for i in range(level_length):
+                node = queue.popleft()
+
+                #if rightmost
+                if i == level_length-1:
+                    rightside.append(node.val)
+
+                #add child nodes 
                 if node.left:
-                    next_level.append(node.left)
+                    queue.append(node.left)
                 if node.right:
-                    next_level.append(node.right)
-                    
-            #current level finished add next level rightmost element
-            rightside.append(node.val)
-            
+                    queue.append(node.right)
         return rightside
+        
+        
+        
+#         if root is None:
+#             return []
+        
+#         next_level = deque([root,])
+#         rightside = []
+        
+#         while next_level:
+#             #prep for next level
+#             curr_level = next_level
+#             next_level = deque()
+            
+#             while curr_level:
+#                 node = curr_level.popleft()
+                
+#                 #add children
+#                 if node.left:
+#                     next_level.append(node.left)
+#                 if node.right:
+#                     next_level.append(node.right)
+                    
+#             #current level finished add next level rightmost element
+#             rightside.append(node.val)
+            
+#         return rightside
