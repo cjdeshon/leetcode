a28224a563c64d2394ed78a86c73fe6eed2dae85
cjdeshon <152043933+cjdeshon@users.noreply.github.com>
2024-12-23 15:27:29 -0600

    Time: 66 ms (6.57%), Space: 20.8 MB (91.82%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,33 +6,84 @@
 #         self.right = None
 
 class Solution:
-    def __init__(self):
-        self.ans = None
+    # def __init__(self):
+    #     self.ans = None
+    
+    #Traversed Flags
+    BOTH_PENDING = 2
+    LEFT_DONE = 1
+    BOTH_DONE = 0
     
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        #stack approach
     
-        #recursive approach
-        def recurse_tree(current_node: TreeNode)-> bool:
-            
-            #if reached the end of a branch, return False
-            if not current_node:
-                return False
-            
-            #Left recursion
-            left = recurse_tree(current_node.left)
-            
-            #Right recusion
-            right = recurse_tree(current_node.right)
+        stack = [(root,Solution.BOTH_PENDING)]
         
-            #check for p or q
-            mid = current_node == p or current_node == q
+        one_node_found = False
+        LCA_index = -1
+        
+        while stack:
+            parent_node, parent_state = stack[-1]
             
-            #if any two of three flags left, right or mid become True
-            if mid + left + right >= 2:
-                self.ans = current_node
+            #if parent_state not equal to BOTH_DONE, do not pop
+            if parent_state != Solution.BOTH_DONE:
                 
-            #Return true if any flag is true
-            return mid or left or right
+                #if both child traversals are pending
+                if parent_state == Solution.BOTH_PENDING:
+                    
+                    #check if current parent_node is either p or q
+                    if parent_node == p or parent_node == q:
+                        
+                        if one_node_found:
+                            return stack[LCA_index][0]
+                        else:
+                            one_node_found = True
+                            #save current top index of stack for LCA
+                            LCA_index = len(stack) - 1
+                    #if both pending, traverse left first
+                    child_node = parent_node.left
+                else:
+                    child_node = parent_node.right
+                    
+                #update node state at top of stack after visiting child
+                stack.pop()
+                stack.append((parent_node, parent_state - 1))
+                
+                #add child node to stack for traversal
+                if child_node:
+                    stack.append((child_node, Solution.BOTH_PENDING))
+                    
+            else:
+                #parent_state of node is both done
+                if one_node_found and LCA_index == len(stack) - 1:
+                    LCA_index -= 1
+                stack.pop()
+                
+        return None
+                
+    
+#         #recursive approach
+#         def recurse_tree(current_node: TreeNode)-> bool:
+            
+#             #if reached the end of a branch, return False
+#             if not current_node:
+#                 return False
+            
+#             #Left recursion
+#             left = recurse_tree(current_node.left)
+            
+#             #Right recusion
+#             right = recurse_tree(current_node.right)
         
-        recurse_tree(root)
-        return self.ans
+#             #check for p or q
+#             mid = current_node == p or current_node == q
+            
+#             #if any two of three flags left, right or mid become True
+#             if mid + left + right >= 2:
+#                 self.ans = current_node
+                
+#             #Return true if any flag is true
+#             return mid or left or right
+        
+#         recurse_tree(root)
+#         return self.ans
